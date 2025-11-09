281a6019d1fa6883548874f20aaf8a80ae9ba99b
Corentin Robac <corentin.robac@laposte.net>
2025-11-09 02:08:01 +0100

    prisma ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,12 @@
-// src/lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-// Pour éviter de recréer le client à chaque hot reload en dev.
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
